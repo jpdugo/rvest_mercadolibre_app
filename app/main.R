f42fec56a899398801e8418a0ab7f79b5017a1cc
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2023-11-05 18:10:47 -0300

    add dependencies and app skeleton

--- app/main.R
+++ app/main.R
@@ -1,25 +1,34 @@
 box::use(
   shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  future[plan, multisession],
+  tidyr[unnest],
+  glue[glue],
+  dplyr[...],
+  bslib[...],
+  bsicons[...]
 )
+
+plan(multisession, workers = 10)
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  page_navbar(
+    title = "Search MercadoLibre",
+    sidebar = NULL,
+    nav_spacer(),
+    nav_panel(
+      "Search",
+      card(full_screen = TRUE, card_header("")),
+      icon = bs_icon("search")
+    ),
+    nav_panel("Compare", icon = bs_icon("layout-split")),
+    nav_panel("About", icon = bs_icon("chat-left-dots"))
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
-    })
   })
 }
