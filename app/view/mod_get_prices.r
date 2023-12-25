cd88f1b791d5e5372df18b03ece3b8846f9bad5e
Juan Patricio Dugo <98497626+jpdugo@users.noreply.github.com>
2023-12-25 21:38:04 +0000

    Refactor mod_download_excel.R and add mod_get_prices.r

---
+++ app/view/mod_get_prices.r
@@ -0,0 +1,19 @@
+box::use(
+  shiny[...],
+  app / logic / scrape_functions[...],
+)
+
+#' @export
+ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    actionButton(ns("get_prices"), "Get Prices")
+  )
+}
+
+#' @export
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+  })
+}
